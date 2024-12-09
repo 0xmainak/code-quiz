a9cc65a41ca4937e528b557b41b8b5913984ef69
0xhimangshu <saikiahimangshu1256@gmail.com>
2024-12-10 00:08:40 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <body className='bg-gray-900 w-screen h-screen'>
+    <h1 className="text-[100px] font-bold text-gray-100">
+        CODE QUIZ
+    </h1>
+    </body>
+  )
+}
+
+export default App
