be47aae24fdef81a6eb8ead99d032767f34c7ace
Anant <anantraj322@gmail.com>
2024-12-12 01:08:20 +0530

    heloo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <body className='bg-gray-900 w-screen h-screen'>
     <h1 className="text-[100px] font-bold text-gray-100">
-        CODE QUIZ
+       code quiz
     </h1>
     </body>
   )
